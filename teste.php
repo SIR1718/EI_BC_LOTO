37347cd8ffc039b1e0a79a3d6f5041348d9370cc
pedromoreira-estg <pmoreira.estg@gmail.com>
2017-09-28 12:00:45 +0100

    primeiro php

---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php 
+$a = 10;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+for ($i=0; $i < $a; $i++) {
+	echo "<p> olá php #".$i."</p>";
+}
+?>
+
+</body>
+</html>
